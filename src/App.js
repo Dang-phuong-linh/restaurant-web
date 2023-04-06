18769f0acb0cf2050d1f945540e5f3323df56fac
Dang-phuong-linh <phuonglinh26112003@gmail.com>
2023-04-07 00:48:04 +0700

    home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.scss";
+import Header from "./components/Header/Header";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="mainImg"></div>
+      <div className="contentWrap topBlock1"></div>
+      <div id="map-block"></div>
+      <div id="footer"></div>
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import EnFlag from "../../assets/flag_en.png";
+const Header = () => {
+  return (
+    <div className="navigation-nav clearfix">
+      <div className="lcol logo-new">PIZZA 4P'S</div>
+      <div className="rcol clearfix">
+        <ul className="gNavi hidden-xs">
+          <li className="gNaviAbout">About</li>
+          <li className="gNaviLocation">Location</li>
+          <li className="gNaviMenu">Menu</li>
+          <li className="gNaviE-invoice">E-invoice</li>
+          <li className="gNaviNews">News</li>
+          <li className="gNaviActivities">Activities</li>
+          <li className="gNaviCareer">Career</li>
+          <li className="gNaviOnlineStore">Online Store</li>
+          <div className="btn-apply btn-res opa hidden-xs "> Reservations</div>
+          <div className="btn-apply opa hidden-xs ">Delivery</div>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
